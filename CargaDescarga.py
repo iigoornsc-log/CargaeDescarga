e70ec5338e04eba48327bd7307b3d21d19f1f6c8
iigoornsc <iguinhosgira@gmail.com>
2026-04-29 19:22:26 -0300

    Update CargaDescarga.py

--- CargaDescarga.py
+++ CargaDescarga.py
@@ -18,7 +18,7 @@ import io
 # 1. CONFIGURAÇÃO DA PÁGINA E CSS (THEME MAGALOG CORPORATIVO)
 # ==========================================================
 st.set_page_config(
-    page_title="R.i.t.m.o | MAGALOG", 
+    page_title="RITMO| MAGALOG", 
     page_icon="https://play-lh.googleusercontent.com/WogWYMVkkivEHGyHAZvKtFZ4F3mklNQI-PQ6vsOMdKTSZWqr7etD9XHuPKIY0NkzZqk=w240-h480-rw", # Pode ser qualquer link de imagem
     layout="wide", 
     initial_sidebar_state="expanded"
